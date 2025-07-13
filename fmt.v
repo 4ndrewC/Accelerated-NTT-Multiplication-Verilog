a5ca2ce138e9612cdac3d23c0b0cfec1b1771181
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2025-07-13 17:20:06 +0800

    Create fmt.v

---
+++ fmt.v
@@ -0,0 +1,16 @@
+`ifndef __FMT__
+`define __FMT__ 
+
+`define WORD 32
+`define DWORD 64
+`define MEMSIZE 65536
+`define REGISTERS 8
+`define OPSIZE 5
+`define MAXSIZE 2048
+`define MAXOUT 1000000
+
+`define MODP 998244353
+`define g 3
+`define g_inv 332748118
+
+`endif
